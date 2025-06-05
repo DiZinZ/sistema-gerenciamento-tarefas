080e8143b2925536a50482f32b2950211f372830
Chaveiro <samuellarroque12@gmail.com>
2025-06-05 17:47:37 -0300

    coloquei os negócios q vc mandou e testei

--- main.c
+++ main.c
@@ -76,3 +76,118 @@ void carregarTarefas(char tarefas[][4][50], int *quantidade) {
     fclose(arquivo);
     printf("Tarefas carregadas com sucesso!\n");
 }
+int main() {
+
+    setlocale(LC_ALL, "Portuguese"); //por algum motivo as letras estão bugadas
+
+    char tarefas[100][4][50]; // 100 tarefas, cada uma com 4 campos (título, descrição, prioridade, status)
+
+    int quantidade = 0;
+
+    int opcao;
+ 
+    do {
+
+        printf("\n  Sistema de Gerenciamento de Tarefas\n");
+
+        printf("1. Cadastrar Tarefa      4. Excluir Tarefa\n");
+
+        printf("2. Listar Tarefas        5. Sair\n");
+
+        printf("3. Editar Tarefa\n");
+
+        printf("Escolha uma opção: ");
+
+        scanf("%d", &opcao);
+
+        getchar(); // Limpar o buffer do teclado
+
+        switch (opcao) {
+
+            case 1:
+
+                cadastrarTarefa(tarefas, quantidade);
+
+                quantidade++;
+
+                break;
+
+            case 2:
+
+                listarTarefas(tarefas, quantidade);
+
+                break;
+
+            case 3:
+
+                if (quantidade > 0) {
+
+                    int indice;
+
+                    printf("Digite o índice da tarefa a ser editada (0 a %d): ", quantidade - 1);
+
+                    scanf("%d", &indice);
+
+                    if (indice >= 0 && indice < quantidade) {
+
+                        editarTarefa(tarefas, indice);
+
+                    } else {
+
+                        printf("Índice inválido!\n");
+
+                    }
+
+                } else {
+
+                    printf("Nenhuma tarefa cadastrada.\n");
+
+                }
+
+                break;
+
+            case 4:
+
+                if (quantidade > 0) {
+
+                    int indice;
+
+                    printf("Digite o índice da tarefa a ser excluída (0 a %d): ", quantidade - 1);
+
+                    scanf("%d", &indice);
+
+                    excluirTarefa(tarefas, &quantidade, indice);
+
+                } else {
+
+                    printf("Nenhuma tarefa cadastrada.\n");
+
+                }
+
+                break;
+
+            case 5:
+
+                SalvarTarefas(tarefas, quantidade);
+
+                printf("Saindo do sistema...\n");
+
+                break;
+
+            default:
+
+                printf("Opção inválida! Tente novamente.\n");
+
+        }
+
+    } while (opcao != 5);
+
+    return 0;
+
+}
+/*
+as opções de edição de tarefas estão se sobrepondo
+as tarefas salvas não estão sendo puxadas pelo programa após o encerramento
+a var quantidade não salva após o encerramento
+vou fazer o sistema de compilação amanhã
+*/
